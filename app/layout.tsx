8a6b0bba187eefb734ae3638eb3bc3accb50d0a5
Joko Santoso <joko74479@gmail.com>
2025-07-30 18:01:09 +0700

    Remove project configuration and assets
    - Deleted package.json to remove project dependencies and scripts.
    - Removed postcss.config.mjs as it is no longer needed.
    - Deleted SVG assets (file.svg, globe.svg, next.svg, vercel.svg, window.svg) and image (me.jpg) from the public directory.
    - Removed TypeScript configuration file (tsconfig.json).

--- app/layout.tsx
+++
@@ -1,7 +0,0 @@
-// app/layout.tsx
-// cukup re-export children saja
-import { ReactNode } from "react";
-
-export default function RootLayout({ children }: { children: ReactNode }) {
-    return children;
-}
--- next.config.ts
+++
@@ -1,7 +0,0 @@
-import { NextConfig } from "next";
-import createNextIntlPlugin from "next-intl/plugin";
-
-const nextConfig: NextConfig = {};
-
-const withNextIntl = createNextIntlPlugin("./lib/i18n/request.ts");
-export default withNextIntl(nextConfig);
